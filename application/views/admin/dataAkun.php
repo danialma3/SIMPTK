5e9f1003d05094db367b2e6608db0e9dd203a3f8
Muhammad Khuzain <mkhuzain17@gmail.com>
2022-05-04 10:39:12 +0800

    add modal warning & admin profile

--- application/views/admin/dataAkun.php
+++ application/views/admin/dataAkun.php
@@ -31,10 +31,33 @@
                                         <td><?= $a["email"]; ?></td>
                                         <td><?= $a["date_create"]; ?></td>
                                         <td>
-                                            <form method="post" action="<?= base_url('admin/dataAkun'); ?>">
-                                                <input type="text" id="email" name="email" value="<?= $a["email"]; ?>" style="display: none;">
-                                                <button type="submit" class="btn btn-danger">Hapus</button>
-                                            </form>
+                                            <?php if ($a["email"] !== $user['email']): ?>
+                                            <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
+                                                Hapus
+                                            </button>
+                                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                <div class="modal-dialog" role="document">
+                                                    <div class="modal-content">
+                                                        <div class="modal-header">
+                                                            <h5 class="modal-title" id="exampleModalLabel">Warning</h5>
+                                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                                                <span aria-hidden="true">&times;</span>
+                                                            </button>
+                                                        </div>
+                                                        <div class="modal-body">
+                                                            Akun akan dihapus. Konfirmasi hapus akun?
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                            <form method="post" action="<?= base_url('admin/dataAkun'); ?>">
+                                                                <input type="text" id="email" name="email" value="<?= $a["email"]; ?>" style="display: none;">
+                                                                <button type="submit" class="btn btn-danger">Hapus</button>
+                                                            </form>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <?php endif; ?>
                                         </td>
                                     </tr>
                                 <?php endforeach; ?>
@@ -45,5 +68,8 @@
             </div>
         </div>
     </div>
+    <style>
+
+    </style>
 
     <!-- /.container-fluid -->
